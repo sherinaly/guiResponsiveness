ca64cda34ed18ab0da171ceaa6c113564c16c95b
Sherin Aly <sherin@vt.edu>
2017-01-16 11:02:28 -0500

    v1

--- guiResponsivness/MainWindow.xaml.cs
+++ guiResponsivness/MainWindow.xaml.cs
@@ -1,94 +1,88 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Threading;
-using System.IO;
-using System.Runtime.CompilerServices;
 
 namespace guiResponsivness
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+        private static string _fileContents;
+
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private  void ReadFile_Click(object sender, RoutedEventArgs e)
-        {
-            
-            Thread thread2 = new Thread(ReadFile);
-            thread2.IsBackground = true;
-            thread2.Start();
-           
+            _fileContents = "";
 
         }
 
-     async void ReadFile()
+     private void ReadFile_Click(object sender, RoutedEventArgs e)
         {
+            var task = Task.Factory.StartNew(ReadFile);
+        }
+
+        private async void ReadFile()
+        {
+            Dispatcher.Invoke(() => loadingLabel.Content = "Loading");
             byte[] results;
             using (var fs = File.Open(Environment.CurrentDirectory + @"\test.txt", FileMode.Open))
             {
                 results = new byte[fs.Length];
-               await  fs.ReadAsync(results, 0, (int) fs.Length);
+                await fs.ReadAsync(results, 0, (int) fs.Length);
+            }
+
+           
+            _fileContents = Encoding.ASCII.GetString(results);
+            Dispatcher.Invoke(() => loadingLabel.Content = "Done Loading");
+        }
+
+        private void WriteOnFile_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Factory.StartNew(WriteToFile);
+        }
+
+        private async void WriteToFile()
+        {
+            if (_fileContents.Length > 0)
+            {
+                var uniEncoding = new UnicodeEncoding();
+                var filename = Environment.CurrentDirectory + @"\testCopy.txt";
+
+                using (var sourceStream = File.Open(filename, FileMode.Create))
+                {
+                    var result = uniEncoding.GetBytes(_fileContents);
+                    sourceStream.Seek(0, SeekOrigin.End);
+                    await sourceStream.WriteAsync(result, 0, result.Length);
+                }
                 Dispatcher.Invoke(() =>
-                 { InputTextBox.Text = Encoding.ASCII.GetString(results); });
+                {
+                    WritingTextBox.Text = _fileContents;
+                    message.Content = "";
+                });
             }
-          //  MessageBox.Show("in reading");
-         
-        }
-        private  void WriteOnFile_Click(object sender, RoutedEventArgs e)
-        {
-            Thread thread = new Thread(WriteToFile);
-            thread.IsBackground = true;
-            thread.Start();
-        }
-
-        async void WriteToFile()
-        {
-            var uniEncoding = new UnicodeEncoding();
-            var filename = Environment.CurrentDirectory + @"\testCopy.txt";
-            var text="";
-            Dispatcher.Invoke(() =>
+            else
             {
-                 text = WritingTextBox.Text;
-                
-            });
-
-            using (var sourceStream = File.Open(filename, FileMode.Create))
-            {
-                var result = uniEncoding.GetBytes(text);
-                sourceStream.Seek(0, SeekOrigin.End);
-                await sourceStream.WriteAsync(result, 0, result.Length);
+                Dispatcher.Invoke(() => message.Content += "Not Done Reading Yet\n");
             }
         }
+
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-          //  for (int i = 0; i < 100; i++)
-            {
-                StatusTetBox.Text = "I am doing Something Else....";
-                
-            }
-           
+           StatusTetBox.Text = "I am doing Something Else....";
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-            InputTextBox.Text = ""; StatusTetBox.Text = "";
+            _fileContents = "";
+            loadingLabel.Content = "";
+            message.Content = "";
+            WritingTextBox.Text = "";
+            StatusTetBox.Text = "";
         }
     }
 }
